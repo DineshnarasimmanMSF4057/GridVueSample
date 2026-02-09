00a3e66f84c6d77d22e3f9805912cbd18d8fbcee
DineshnarasimmanMSF4057 <125851531+DineshnarasimmanMSF4057@users.noreply.github.com>
2026-02-09 19:20:59 +0530

    fix: enable vue plugin + base for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
-  },
-  base: '/VUE3-JS-GRID/',
-});
+  base: '/GridVueSample/',   // <-- your repo name
+})
